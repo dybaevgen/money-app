164ef7b909918414f6285a285dfaa31f11f84096
dybaevgen <dybaevgen50@gmail.com>
2026-08-29 22:36:28 +0200

    VV

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE = 'money-pwa-v4.1.0';
+const CACHE = 'money-pwa-v4.2.0';
 const ASSETS = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
+  './style.css?v=4.2.0',
+  './app.js?v=4.2.0',
   './manifest.json',
   './icons/icon-180.png',
   './icons/icon-192.png',
